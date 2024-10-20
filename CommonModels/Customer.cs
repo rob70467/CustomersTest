b9b9721c303642fbef7fdbca6247e12141bbb3b4
rob70467 <mrob67@hotmail.com>
2024-10-20 16:39:54 +0100

    Add files via upload

---
+++ CommonModels/Customer.cs
@@ -0,0 +1,28 @@
+﻿namespace CommonModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Text.Json.Serialization;
+    using System.Threading.Tasks;
+    using System.Xml.Linq;
+
+    public class Customer
+    {
+        [JsonInclude]
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonInclude]
+        [JsonPropertyName("name")]
+        [Display(Name = "Customer Name")]
+        public string Name { get; set; }
+
+        [JsonInclude]
+        [JsonPropertyName("reference")]
+        [Display(Name = "Reference")]
+        public string Reference { get; set; }
+    }
+}
